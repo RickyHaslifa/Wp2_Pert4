6e6c5cbb0b1e37f02fa57a6be9f2569f5b4ad377
RickyHaslifa <90895840+RickyHaslifa@users.noreply.github.com>
2021-10-29 15:27:29 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Ricky</li>
+        <li>Nama Belakang : Haslifa</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> PUP BLOK DD 1/ NO 3 BEKASI UTARA</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Basket</li>
+        <li>Bulutangkis</li>
+    </ul>
+</section>
